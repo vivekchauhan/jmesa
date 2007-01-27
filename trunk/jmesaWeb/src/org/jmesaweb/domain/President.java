2741f3dc347d3d8746cd75d225cdc4dea2a6e07b
extremecomponents <extremecomponents@07444f95-541f-0410-993b-35a84c1256c0>
2007-01-27 17:27:42 +0000

    initial checkin

---
+++ trunk/jmesaWeb/src/org/jmesaweb/domain/President.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2004 original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jmesaweb.domain;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+/**
+ * @author Jeff Johnston
+ */
+public class President implements Serializable {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String nickName;
+    private String term;
+    private Date born;
+    private Date died;
+    private String education;
+    private String career;
+    private String politicalParty;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getBorn() {
+        return born;
+    }
+
+    public void setBorn(Date born) {
+        this.born = born;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    public Date getDied() {
+        return died;
+    }
+
+    public void setDied(Date died) {
+        this.died = died;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPoliticalParty() {
+        return politicalParty;
+    }
+
+    public void setPoliticalParty(String politicalParty) {
+        this.politicalParty = politicalParty;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+}
---
+++ trunk/jmesaWeb/web/jsp/example.jsp
@@ -0,0 +1,52 @@
+<html>
+
+<head>
+	<title>JMesa Example</title>
+</head>
+
+<body>
+
+	<p class="content">
+		JMesa In Action. 
+	</p>
+	
+	
+	<form name="presidentsForm" action="${pageContext.request.contextPath}/presidents.run">
+		${presidents}
+	</form>
+	
+	<p class="content">
+		The example code using the TableFactory. See the wiki for the complete 
+		<a href="http://code.google.com/p/jmesa/wiki/Example">source code</a>.
+	</p>
+	
+<pre>
+HtmlTableFactory tableFactory = new HtmlTableFactory(webContext, coreContext);
+
+HtmlTable table = tableFactory.createTable("firstName", "lastName", "term", "career");
+table.setCaption("Presidents");
+table.getTableRenderer().setWidth("600px;");
+
+CellEditor editor = new PresidentsLinkEditor(new BasicCellEditor());
+table.getRow().getColumn("firstName").getCellRenderer().setCellEditor(editor);
+
+ToolbarFactory toolbarFactory = new ToolbarFactoryImpl(table, webContext, coreContext, "csv");
+Toolbar toolbar = toolbarFactory.createToolbar();
+View view = new HtmlView(table, toolbar, coreContext);
+</pre>
+
+<script type="text/javascript">
+function onInvokeAction(id) {
+	setExportToLimit(id, '');
+	createHiddenInputFieldsForLimitAndSubmit(id);
+}
+function onInvokeExportAction(id) {
+	var parameterString = createParameterStringForLimit(id, true);
+	location.href = '${pageContext.request.contextPath}/presidents.run?' + parameterString;
+}
+</script>
+
+</body>
+
+</html>
+
