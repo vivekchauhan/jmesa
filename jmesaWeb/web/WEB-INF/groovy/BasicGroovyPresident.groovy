8db34b49e6f1a5b5767a0dbad22a22c7ad725508
extremecomponents <extremecomponents@07444f95-541f-0410-993b-35a84c1256c0>
2007-04-30 22:14:20 +0000

    Added Groovy support in the form of dealing with closures with CellEditor level.

---
+++ jmesaWeb/web/WEB-INF/groovy/BasicGroovyPresident.groovy
@@ -0,0 +1,64 @@
+import java.util.Collection;
+
+import org.jmesa.core.CoreContext;
+import org.jmesa.core.CoreContextFactory;
+import org.jmesa.core.CoreContextFactoryImpl;
+import org.jmesa.limit.Limit;
+import org.jmesa.limit.LimitFactory;
+import org.jmesa.limit.LimitFactoryImpl;
+import org.jmesa.view.View;
+import org.jmesa.view.component.Column;
+import org.jmesa.view.html.HtmlTableFactory;
+import org.jmesa.view.html.HtmlView;
+import org.jmesa.view.html.component.HtmlTable;
+import org.jmesa.view.html.toolbar.Toolbar;
+import org.jmesa.view.html.toolbar.ToolbarFactoryImpl;
+import org.jmesa.web.WebContext;
+
+import org.jmesa.view.editor.CellEditor;
+import org.jmesa.view.editor.BasicCellEditor;
+
+class BasicGroovyPresident {
+    String id;
+    int maxRows;
+    
+	String getHtmlTable(Collection items, WebContext webContext) {
+        def limitFactory = [id, webContext] as LimitFactoryImpl;
+        def limit = limitFactory.createLimitAndRowSelect(maxRows, items.size());
+
+        def factory = [webContext] as CoreContextFactoryImpl;
+        def coreContext = factory.createCoreContext(items, limit);
+
+        return render(webContext, coreContext).toString();
+	}
+	
+    Object render(WebContext webContext, CoreContext coreContext) {
+        def tableFactory = [webContext, coreContext] as HtmlTableFactory;
+        def columns = ["name.firstName", "name.lastName", "term", "career"] as String[];
+        def table = tableFactory.createHtmlTable(columns); // Must use createHtmlTable because createTable 
+                                                           // does not work because of Groovy bug [GROOVY-1860].
+        table.caption = "Presidents";
+        table.tableRenderer.width = "600px";
+
+        def firstName = table.row.getColumn("name.firstName");
+        firstName.title = "First Name";
+
+        firstName.headerRenderer.defaultSortOrderable = false;
+		firstName.cellRenderer.setCellEditor({item, property, rowcount -> 
+		 	def value = new BasicCellEditor().getValue(item, property, rowcount);
+            return """
+                    <a href="http://www.whitehouse.gov/history/presidents/">
+                       $value
+                    </a>
+                   """});
+
+        def lastName = table.row.getColumn("name.lastName");
+        lastName.title = "Last Name";
+
+        def toolbarFactory = [table, webContext, coreContext] as ToolbarFactoryImpl;
+        def toolbar = toolbarFactory.createToolbar();
+        def view = [table, toolbar, coreContext] as HtmlView;
+
+        return view.render();
+    }
+}
