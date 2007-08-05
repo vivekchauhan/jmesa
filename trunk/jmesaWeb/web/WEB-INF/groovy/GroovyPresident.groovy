99fa3f18a0848982ed2e8e6a191650e392dec25d
extremecomponents <extremecomponents@07444f95-541f-0410-993b-35a84c1256c0>
2007-08-05 13:53:17 +0000

    Updated the Groovy example.

--- trunk/jmesaWeb/web/WEB-INF/groovy/GroovyPresident.groovy
+++ trunk/jmesaWeb/web/WEB-INF/groovy/GroovyPresident.groovy
@@ -1,61 +1,48 @@
 import org.jmesaweb.controller.HtmlTableTemplate;
 
 import java.util.Collection;
-import org.jmesa.core.CoreContext;
-import org.jmesa.core.CoreContextFactory;
-import org.jmesa.core.CoreContextFactoryImpl;
-import org.jmesa.limit.Limit;
-import org.jmesa.limit.LimitFactory;
-import org.jmesa.limit.LimitFactoryImpl;
-import org.jmesa.view.View;
-import org.jmesa.view.component.Column;
-import org.jmesa.view.html.HtmlTableFactory;
-import org.jmesa.view.html.HtmlView;
-import org.jmesa.view.html.component.HtmlTable;
-import org.jmesa.view.html.toolbar.Toolbar;
-import org.jmesa.view.html.toolbar.ToolbarFactoryImpl;
-import org.jmesa.web.WebContext;
 
-import org.jmesa.view.editor.CellEditor;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jmesa.limit.Limit;
+import org.jmesa.view.TableFacade;
+import org.jmesa.view.TableFacadeImpl;
+import org.jmesa.view.component.Column;
+import org.jmesa.view.component.Table;
 import org.jmesa.view.editor.BasicCellEditor;
+import org.jmesa.view.editor.CellEditor;
+import org.jmesa.view.html.HtmlBuilder;
+import org.jmesa.view.html.component.HtmlTable;
+import org.jmesaweb.controller.HtmlTableTemplate;
 
 class BasicGroovyPresident implements HtmlTableTemplate {
     String id;
     int maxRows;
     
-	String build(Collection items, WebContext webContext) {
-        def limitFactory = [id, webContext] as LimitFactoryImpl;
-        def limit = limitFactory.createLimitAndRowSelect(maxRows, items.size());
+	String build(Collection<Object> items, HttpServletRequest request) {
+        def facade = [id, request, maxRows, items, "name.firstName", "name.lastName", "term", "career"] as TableFacadeImpl
 
-        def factory = [webContext] as CoreContextFactoryImpl;
-        def coreContext = factory.createCoreContext(items, limit);
+        def table = facade.table
+        table.caption = "Presidents"
 
-        def tableFactory = [webContext, coreContext] as HtmlTableFactory;
-        def columns = ["name.firstName", "name.lastName", "term", "career"] as String[];
-        def table = tableFactory.createHtmlTable(columns); // Must use createHtmlTable because createTable 
-                                                           // does not work because of Groovy bug [GROOVY-1860].
-        table.caption = "Presidents";
-        table.tableRenderer.width = "600px";
+        def firstName = table.row.getColumn("name.firstName")
+        firstName.title = "First Name"
 
-        def firstName = table.row.getColumn("name.firstName");
-        firstName.title = "First Name";
+        def lastName = table.row.getColumn("name.lastName")
+        lastName.title = "Last Name"
 
-        firstName.headerRenderer.defaultSortOrderable = false;
+        table.tableRenderer.width = "600px"
+
+        firstName.headerRenderer.defaultSortOrderable = false
 		firstName.cellRenderer.setCellEditor({item, property, rowcount -> 
 		 	def value = new BasicCellEditor().getValue(item, property, rowcount);
             return """
                     <a href="http://www.whitehouse.gov/history/presidents/">
                        $value
                     </a>
-                   """});
+                   """})
 
-        def lastName = table.row.getColumn("name.lastName");
-        lastName.title = "Last Name";
-
-        def toolbarFactory = [table, webContext, coreContext] as ToolbarFactoryImpl;
-        def toolbar = toolbarFactory.createToolbar();
-        def view = [table, toolbar, coreContext] as HtmlView;
-
-        return view.render().toString();
+        return facade.render()
     }
 }
