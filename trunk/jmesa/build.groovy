1684164a9daa3e85abfc6785d0664507ee225bec
extremecomponents <extremecomponents@07444f95-541f-0410-993b-35a84c1256c0>
2007-11-23 20:27:01 +0000

    modified the naming

--- trunk/jmesa/build.groovy
+++ trunk/jmesa/build.groovy
@@ -178,18 +178,13 @@ class Build {
      * Retrieve the dependant jars from the ivy repository and put 
      * them in the lib folder of the project
      */
-    static jars() {
-		def ant = [] as AntBuilder
-		def libDir = 'lib'	
-		
-		ant.mkdir(dir:libDir)
-	
-		def ivy = [ant:ant, namespace:'fr.jayasoft.ivy.ant'] as AntLibHelper
-		ant.property(file:'ivy.properties')
-		ivy.configure(file:'ivyconf.xml')
-		ivy.resolve(file:'ivy.xml')
-		ivy.retrieve(pattern:"$libDir/[artifact]-[revision].[ext]", sync:true, conf:'test')
-	}    
+    def lib() {
+        def libDir = 'lib'	
+	ant.mkdir(dir:libDir)
+	ivy.configure(file:'ivyconf.xml')
+	ivy.resolve(file:'ivy.xml')
+	ivy.retrieve(pattern:"$libDir/[artifact]-[revision].[ext]", sync:true, conf:'test')
+    }    
     
     def dist() {
         clean()
@@ -212,7 +207,7 @@ class Build {
     static void main(args) {
         def cli = new CliBuilder(usage:'groovy build.groovy -[ha]')
         cli.h(longOpt: 'help', 'usage information')
-        cli.a(argName:'action', longOpt:'action', args:1, required:true, 'action(s) [dist, clean, jars]')
+        cli.a(argName:'action', longOpt:'action', args:1, required:true, 'action(s) [dist, clean, lib]')
         
         def options = cli.parse(args)
         
@@ -221,7 +216,7 @@ class Build {
             return
         }
 
-        def build = new Build(options.a == 'clean' || options.a == 'jars')
+        def build = new Build(options.a == 'clean' || options.a == 'lib')
         def action = options.a
         build.invokeMethod(action, null)
     }
