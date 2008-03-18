82a79e25760630da67366c9160ec2790ba2ad724
extremecomponents <extremecomponents@07444f95-541f-0410-993b-35a84c1256c0>
2008-03-18 01:53:50 +0000

    Added another Locale.

--- trunk/jmesaWeb/src/org/jmesaweb/dao/PresidentDaoImpl.java
+++ trunk/jmesaWeb/src/org/jmesaweb/dao/PresidentDaoImpl.java
@@ -20,7 +20,6 @@ import java.util.HashMap;
 import java.util.List;
 
 import java.util.Map;
-import org.apache.commons.beanutils.PropertyUtils;
 import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
