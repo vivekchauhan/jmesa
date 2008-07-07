334630160c6d32134f4e3a85c5c28ecfbc7c9ef9
extremecomponents <extremecomponents@07444f95-541f-0410-993b-35a84c1256c0>
2008-07-07 17:45:03 +0000

    A spring implementation to get the messages.

--- trunk/jmesa/src/org/jmesa/core/message/SpringMessages.java
+++ trunk/jmesa/src/org/jmesa/core/message/SpringMessages.java
@@ -16,38 +16,47 @@
 package org.jmesa.core.message;
 
 import org.jmesa.web.WebContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 /**
  * The Spring specific messages. Will use the default messages if they are not defined in Spring.
  *
  * @since 2.3.3
- * @author Jeff Johnston
+ * @author Oscar Perez
  */
 public class SpringMessages implements Messages {
 
+    private static Logger logger = LoggerFactory.getLogger(SpringMessages.class);
     private Messages defaultMessages;
     private WebContext webContext;
+    private MessageSource messageSource;
 
     public SpringMessages(Messages defaultMessages, WebContext webContext) {
 
         this.defaultMessages = defaultMessages;
         this.webContext = webContext;
-        
-        //WebApplicationContext webApplicationContext = WebApplicationContextUtils.getWebApplicationContext(webContext.getServletContext());
-        //webApplicationContext.getBean("messageSource");
+        this.messageSource = WebApplicationContextUtils.getWebApplicationContext(webContext.getServletContext());
     }
 
+    /**
+     * Try to get the messages from Spring first or else retrieve from the default messages.
+     */
     public String getMessage(String code) {
-
-        // Try to get the messages from Spring first.
-
-        return defaultMessages.getMessage(code);
+        return getMessage(code, null);
     }
 
+    /**
+     * Try to get the messages from Spring first or else retrieve from the default messages.
+     */
     public String getMessage(String code, Object[] args) {
+        if (messageSource == null) {
+            logger.warn("There is no Spring MessageSource defined. Will get the default messages instead.");
+            return defaultMessages.getMessage(code, args);
+        }
 
-        // Try to get the messages from Spring first.
-
-        return defaultMessages.getMessage(code, args);
+        return messageSource.getMessage(code, args, defaultMessages.getMessage(code, args), webContext.getLocale());
     }
 }
