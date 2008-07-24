9beffbb918a1576b5441875ff432f85aaa5adbc4
extremecomponents <extremecomponents@07444f95-541f-0410-993b-35a84c1256c0>
2008-07-24 03:02:11 +0000

    Have a new pdf view.

--- jmesa/src/org/jmesa/view/pdfp/PdfPView.java
+++ jmesa/src/org/jmesa/view/pdfp/PdfPView.java
@@ -37,7 +37,7 @@ import org.jmesa.web.WebContext;
  * A PDF view that uses the iText PdfPTable.
  *
  * @since 2.3.4
- * @author acimasiz
+ * @author Ismail
  */
 public class PdfPView implements View {
 
