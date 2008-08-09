6d539ca6aff9e5057772884fcbc7855ddfeadfb4
extremecomponents <extremecomponents@07444f95-541f-0410-993b-35a84c1256c0>
2008-08-09 16:37:59 +0000

    using the new jmesa.js file

--- trunk/jmesaWeb/web/js/jquery.jmesa.js
+++ trunk/jmesaWeb/web/js/jquery.jmesa.js
@@ -16,7 +16,7 @@
             events : {}
         };
     }
-	
+
     // Unlike previous version of jmesa.js, this class does not store state,
     // it is simply an accessor.
     var TableFacadeManager = {
@@ -127,7 +127,7 @@
             this.property = property;
         }
     });
-	
+
     $.extend(classes.Limit.prototype, {
         getId : function () {
             return this.id;
@@ -172,91 +172,91 @@
             this.exportType = exportType;
         }
     });
-	
+
     $.extend(classes.TableFacade.prototype, {
         createHiddenInputFields : function (form) {
             var limit = this.limit;
-	    
+
             var exists = $(form).find(':hidden[@name=' + limit.id + '_p_]').val();
             if (exists) {
                 return false;
             }
-		    
+
             if (this.worksheet.save) {
                 $(form).append('<input type="hidden" name="' + limit.id + '_sw_" value="true"/>');
             }
-		    
+
             if (this.worksheet.filter) {
                 $(form).append('<input type="hidden" name="' + limit.id + '_fw_" value="true"/>');
             }
-		    
+
             // tip the API off that in the loop of working with the table
             $(form).append('<input type="hidden" name="' + limit.id + '_tr_" value="true"/>');
-		    
+
             // the current page
             $(form).append('<input type="hidden" name="' + limit.id + '_p_" value="' + limit.page + '"/>');
             $(form).append('<input type="hidden" name="' + limit.id + '_mr_" value="' + limit.maxRows + '"/>');
-		    
+
             // the sort objects
             var sortSet = limit.getSortSet();
             $.each(sortSet, function (index, sort) {
                 $(form).append('<input type="hidden" name="' + limit.id + '_s_'  + sort.position + '_' + sort.property + '" value="' + sort.order + '"/>');
             });
-		    
+
             // the filter objects
             var filterSet = limit.getFilterSet();
             $.each(filterSet, function (index, filter) {
                 $(form).append('<input type="hidden" name="' + limit.id + '_f_' + filter.property + '" value="' + filter.value + '"/>');
             });
-		    
+
             return true;
         },
         createParameterString : function () {
             var limit = this.limit;
-		    
+
             var url = '';
-		    
+
             // the current page
             url += limit.id + '_p_=' + limit.page;
-		    
+
             // the max rows
             url += '&' + limit.id + '_mr_=' + limit.maxRows;
-		    
+
             // the sort objects
             var sortSet = limit.getSortSet();
             $.each(sortSet, function (index, sort) {
                 url += '&' + limit.id + '_s_' + sort.position + '_' + sort.property + '=' + sort.order;
             });
-		    
+
             // the filter objects
             var filterSet = limit.getFilterSet();
             $.each(filterSet, function (index, filter) {
                 url += '&' + limit.id + '_f_' + filter.property + '=' + encodeURIComponent(filter.value);
             });
-		    
+
             // the export
             if (limit.exportType) {
                 url += '&' + limit.id + '_e_=' + limit.exportType;
             }
-		    
+
             // tip the API off that in the loop of working with the table
             url += '&' + limit.id + '_tr_=true';
-		    
+
             if (this.worksheet.save) {
                 url += '&' + limit.id + '_sw_=true';
             }
-		    
+
             if (this.worksheet.filter) {
                 url += '&' + limit.id + '_fw_=true';
             }
-		    
+
             return url;
         }
     });
 
     // Placing the dynFilter and wsColumn globals in this namespace is a
     // breaking change. I do not think that client code really uses this,
-    // but in case it does, that code can be modified to access $.jmesa instead.
+    // but in case it does,	that code can be modified to access $.jmesa instead.
     $.extend(functions, {
         getAllTables : function () {
             return $('#' + TableFacadeManager.getTableIds().join(',#'));
@@ -274,9 +274,9 @@
 
             var input = $('#dynFilterInput');
             input.focus();
-		    
+
             var table = $('#' + id);
-		    
+
             $(input).keypress(function (event) {
                 if (event.keyCode === 13) { // press the enter key
                     var changedValue = input.val();
@@ -286,7 +286,7 @@
                     $.jmesa.dynFilter = null;
                 }
             });
-		    
+
             $(input).blur(function () {
                 var changedValue = input.val();
                 cell.html(changedValue);
@@ -298,20 +298,20 @@
             if ($.jmesa.dynFilter) {
                 return;
             }
-		
+
             $.jmesa.dynFilter = new classes.DynFilter(filter, id, property);
-		    
+
             if ($('#dynFilterDroplistDiv').size() > 0) {
                 return; // filter already created
             }
-		    
+
             // The cell that represents the filter.
             var cell = $(filter);
-		    
+
             // Get the original filter value and width.
             var originalValue = cell.text();
             var width = cell.width();
-		    
+
             // Need to first get the size of the arrary.
             var size = 1;
             $.each(options, function () {
@@ -321,10 +321,10 @@
                     return false;
                 }
             });
-		    
+
             // Create the dynamic select input box.
             cell.html('<div id="dynFilterDroplistDiv" style="top:17px">');
-		    
+
             var html = '<select id="dynFilterDroplist" name="filter" size="' + size + '">';
             html += '<option value=""> </option>';
             $.each(options, function (key, value) {
@@ -334,31 +334,31 @@
                     html += '<option value="' + key + '">' + value + '</option>';
                 }
             });
-		    
+
             html += '</select>';
-		
+
             var div = $('#dynFilterDroplistDiv');
             div.html(html);
-		
+
             var input = $('#dynFilterDroplist');
-		
+
             // Resize the column if it is not at least as wide as the column.
             var selectWidth = input.width();
             if (selectWidth < width) {
                 input.width(width + 5); // always make the droplist overlap some
             }
-		
+
             input.focus();
-		
+
             var originalBackgroundColor = cell.css("backgroundColor");
             cell.css({
                 backgroundColor : div.css("backgroundColor")
             });
-		
+
             // Something was selected or the clicked off the droplist.
-		
+
             var table = $('#' + id);
-		
+
             $(input).change(function () {
                 var changedValue = $("#dynFilterDroplistDiv option:selected").val();
                 cell.text(changedValue);
@@ -366,7 +366,7 @@
                 table.invokeAction();
                 $.jmesa.dynFilter = null;
             });
-		
+
             $(input).blur(function () {
                 var changedValue = jQuery("#dynFilterDroplistDiv option:selected").val();
                 cell.text(changedValue);
@@ -386,7 +386,7 @@
             .css({
                 'background': 'url(' + imagesPath + 'shadow_back.gif) 100% repeat'
                 });
-		    
+
             $('.' + theme + ' div.wrap0').css({
                 'background' : 'url(' + imagesPath + 'shadow.gif) right bottom no-repeat',
                 'float' : 'left'
@@ -400,27 +400,27 @@
             $('.' + theme + ' div.dropShadow').css({
                 'background': 'url(' + imagesPath + 'corner_tr.gif) 100% -18px no-repeat'
                 });
-		    
+
             $('div.' + theme).append('<div style="clear:both">&nbsp;</div>');
         },
         createWsColumn : function (column, id, uniqueProperties, property) {
             if ($.jmesa.wsColumn) {
                 return;
             }
-		
+
             $.jmesa.wsColumn = new classes.WsColumn(column, id, uniqueProperties, property);
-		    
+
             var cell = jQuery(column);
             var width = cell.width();
             var originalValue = cell.text();
-		    
+
             cell.parent().width(width); // set the outer width to avoid dynamic column width changes
-		    
+
             cell.html('<div id="wsColumnDiv"><input id="wsColumnInput" name="column" style="width:' + (width + 1) + 'px" value="' + originalValue + '"/></div>');
-		    
+
             var input = jQuery('#wsColumnInput');
             input.focus();
-		    
+
             $('#wsColumnInput').keypress(function (event) {
                 if (event.keyCode === 13) { // press the enter key
                     var changedValue = input.val();
@@ -431,7 +431,7 @@
                     $.jmesa.wsColumn = null;
                 }
             });
-		    
+
             $('#wsColumnInput').blur(function () {
                 var changedValue = input.val();
                 cell.html(changedValue);
@@ -444,38 +444,38 @@
         submitWsCheckboxColumn : function (column, id, uniqueProperties, property) {
 
             $.jmesa.wsColumn = new classes.WsColumn(column, id, uniqueProperties, property);
-		    
+
             var checked = column.checked;
-		    
+
             var changedValue = 'unchecked';
             if (checked) {
                 changedValue = 'checked';
             }
-		    
+
             var originalValue = 'unchecked';
             if (!checked) { // the first time the original value is the opposite
                 originalValue = 'checked';
             }
-		    
+
             functions.submitWsColumn(originalValue, changedValue);
-		    
+
             $.jmesa.wsColumn = null;
         },
         submitWsColumn : function (originalValue, changedValue) {
             var data = '{ "id" : "' + $.jmesa.wsColumn.id + '"';
-		    
+
             data += ', "cp_" : "' + $.jmesa.wsColumn.property + '"';
-		    
+
             var props = $.jmesa.wsColumn.uniqueProperties;
             $.each(props, function (key, value) {
                 data += ', "up_' + key + '" : "' + value + '"';
             });
-		    
+
             data += ', "ov_" : "' + originalValue + '"';
             data += ', "cv_" : "' + changedValue + '"';
-		    
+
             data += '}';
-		    
+
             $.post('jmesa.wrk?', eval('(' + data + ')'), function (data) {});
         }
     });
@@ -515,7 +515,7 @@
             // and then set the page back to the first one.
             functions.removeSortFromLimit(id, property);
             functions.setPageToLimit(id, '1');
-		    
+
             var limit = TableFacadeManager.getTableFacade(id).limit;
             var sort = new classes.Sort(position, property, order);
             limit.addSort(sort);
@@ -540,7 +540,7 @@
             // and then set the page back to the first one.
             functions.removeFilterFromLimit(id, property);
             functions.setPageToLimit(id, '1');
-		    
+
             var limit = TableFacadeManager.getTableFacade(id).limit;
             var filter = new classes.Filter(property, value);
             limit.addFilter(filter);
@@ -557,11 +557,11 @@
         },
         removeAllFiltersFromLimit : function (id) {
             var tableFacade = TableFacadeManager.getTableFacade(id);
-		    
+
             var limit = tableFacade.limit;
             limit.setFilterSet([]);
             functions.setPageToLimit(id, '1');
-		    
+
             var worksheet = tableFacade.worksheet;
             worksheet.filter = null;
         },
@@ -582,7 +582,7 @@
             }
         }
     };
-	
+
     // Like the chainable functions, these get copied as jQuery functions and
     // will receive the table ID as the first parameter.  However, these
     // functions are meant to return some value other than the jQuery object,
@@ -622,10 +622,10 @@
             return retval;
         }
     };
-	
+
     // Centralize all of the functions on a single object.
     $.extend(functions, chainable, nonchainable);
-	
+
     // Create a JMesa namespace and store the functions and classes in a way
     // that they can be added to the global namespace if necessary.
     $.jmesa = $.extend({
@@ -643,10 +643,10 @@
         hasJMesaEvent : function (name) {
             var table = this.get(0);
             var events = TableFacadeManager.getEventsFor(table.id);
-            return $.isFunction(events)[table.id];
+            return $.isFunction(events)[id];
         }
     };
-	
+
     // Adds support for onInvokeAction and onInvokeExportAction
     $.each(['invokeAction', 'invokeExportAction'], function (i, name) {
         jmesaFunctions[name] = function (fnOrAction) {
@@ -661,7 +661,7 @@
             });
         };
     });
-	
+
     // Iterate through the functions and add them as jQuery-ish methods.
     // This assumes that each of the chainable functions takes the table ID
     // as the first argument, and it passes all the caller's arguments
@@ -689,8 +689,8 @@
     });
 
     $.fn.extend(jmesaFunctions);
-	
+
     // An alias for the $.jmesa namespace (JMesa static functions and classes)
     document.jmesa = $.jmesa;
-	
+
 })(jQuery);
