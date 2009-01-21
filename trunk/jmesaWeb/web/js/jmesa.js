58ee27c55a07047fde6d052278a96ac076155bd1
jeff.johnston.mn <jeff.johnston.mn@07444f95-541f-0410-993b-35a84c1256c0>
2009-01-21 19:09:27 +0000

    Fixed a bug fix with jQuery.

--- trunk/jmesaWeb/web/js/jmesa.js
+++ trunk/jmesaWeb/web/js/jmesa.js
@@ -2,103 +2,103 @@
 /*********** Convenience API Methods ***********/
 
 function addTableFacadeToManager(id) {
-    $.jmesa.addTableFacade(id);
+    jQuery.jmesa.addTableFacade(id);
 }
 
 function setSaveToWorksheet(id) {
-    $.jmesa.setSaveToWorksheet(id);
+    jQuery.jmesa.setSaveToWorksheet(id);
 }
 
 function setFilterToWorksheet(id) {
-    $.jmesa.setFilterToWorksheet(id);
+    jQuery.jmesa.setFilterToWorksheet(id);
 }
 
 function removeFilterFromWorksheet(id) {
-    $.jmesa.removeFilterFromWorksheet(id);
+    jQuery.jmesa.removeFilterFromWorksheet(id);
 }
 
 function setPageToLimit(id, page) {
-    $.jmesa.setPageToLimit(id, page);
+    jQuery.jmesa.setPageToLimit(id, page);
 }
 
 function setMaxRowsToLimit(id, maxRows) {
-    $.jmesa.setMaxRowsToLimit(id, maxRows);
+    jQuery.jmesa.setMaxRowsToLimit(id, maxRows);
 }
 
 function addSortToLimit(id, position, property, order) {
-    $.jmesa.addSortToLimit(id, position, property, order);
+    jQuery.jmesa.addSortToLimit(id, position, property, order);
 }
 
 function removeSortFromLimit(id, property) {
-    $.jmesa.removeSortFromLimit(id, property);
+    jQuery.jmesa.removeSortFromLimit(id, property);
 }
 
 function removeAllSortsFromLimit(id) {
-    $.jmesa.removeAllSortsFromLimit(id);
+    jQuery.jmesa.removeAllSortsFromLimit(id);
 }
 
 function getSortFromLimit(id, property) {
-    $.jmesa.getSortFromLimit(id, property);
+    jQuery.jmesa.getSortFromLimit(id, property);
 }
 
 function addFilterToLimit(id, property) {
-    $.jmesa.addFilterToLimit(id, property);
+    jQuery.jmesa.addFilterToLimit(id, property);
 }
 
 function removeFilterFromLimit(id, property) {
-    $.jmesa.removeFilterFromLimit(id, property);
+    jQuery.jmesa.removeFilterFromLimit(id, property);
 }
 
 function removeAllFiltersFromLimit(id) {
-    $.jmesa.removeAllFiltersFromLimit(id);
+    jQuery.jmesa.removeAllFiltersFromLimit(id);
 }
 
 function getFilterFromLimit(id, property) {
-    $.jmesa.getFilterFromLimit(id, property);
+    jQuery.jmesa.getFilterFromLimit(id, property);
 }
 
 function setExportToLimit(id, exportType) {
-    $.jmesa.setExportToLimit(id, exportType);
+    jQuery.jmesa.setExportToLimit(id, exportType);
 }
 
 function createHiddenInputFieldsForLimit(id) {
-    $.jmesa.createHiddenInputFieldsForLimit(id);
+    jQuery.jmesa.createHiddenInputFieldsForLimit(id);
 }
 
 function createHiddenInputFieldsForLimitAndSubmit(id) {
-    $.jmesa.createHiddenInputFieldsForLimitAndSubmit(id);
+    jQuery.jmesa.createHiddenInputFieldsForLimitAndSubmit(id);
 }
 
 function createParameterStringForLimit(id) {
-    return $.jmesa.createParameterStringForLimit(id);
+    return jQuery.jmesa.createParameterStringForLimit(id);
 }
 
 /*********** Filter ***********/
 
 function createDynFilter(filter, id, property) {
-    $.jmesa.createDynFilter(filter, id, property);
+    jQuery.jmesa.createDynFilter(filter, id, property);
 }
 
 function createDynDroplistFilter(filter, id, property, options) {
-    $.jmesa.createDroplistDynFilter(filter, id, property, options);
+    jQuery.jmesa.createDroplistDynFilter(filter, id, property, options);
 }
 
 /*********** Worksheet ***********/
 
 function createWsColumn(column, id, uniqueProperties, property) {
-    $.jmesa.createWsColumn(column, id, uniqueProperties, property);
+    jQuery.jmesa.createWsColumn(column, id, uniqueProperties, property);
 }
 
 function submitWsCheckboxColumn(column, id, uniqueProperties, property) {
-    $.jmesa.submitWsCheckboxColumn(column, id, uniqueProperties, property);
+    jQuery.jmesa.submitWsCheckboxColumn(column, id, uniqueProperties, property);
 }
 
 function submitWsColumn(originalValue, changedValue) {
-    $.jmesa.submitWsColumn(originalValue, changedValue);   
+    jQuery.jmesa.submitWsColumn(originalValue, changedValue);
 }
 
 /*********** Special Effects ***********/
 
 function addDropShadow(imagesPath, theme) {
-    $.jmesa.addDropShadow(imagesPath, theme);
+    jQuery.jmesa.addDropShadow(imagesPath, theme);
 }
