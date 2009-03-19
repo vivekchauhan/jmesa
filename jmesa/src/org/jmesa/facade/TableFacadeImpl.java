82e74bae44301d387a2af04203cb015bde913abb
jeff.johnston.mn <jeff.johnston.mn@07444f95-541f-0410-993b-35a84c1256c0>
2009-03-19 03:52:06 +0000

    Fixed a bug with the Limit and State feature.

--- jmesa/src/org/jmesa/facade/TableFacadeImpl.java
+++ jmesa/src/org/jmesa/facade/TableFacadeImpl.java
@@ -181,6 +181,10 @@ public class TableFacadeImpl implements TableFacade {
 
         if (l.isComplete()) {
             this.limit = l;
+            if (items != null) {
+                int page = l.getRowSelect().getPage();
+                l.setRowSelect(new RowSelectImpl(page, getMaxRows(), items.size()));
+            }
             return limit;
         }
 
