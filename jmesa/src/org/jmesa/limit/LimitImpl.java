4651e245d2302a7b24be9dff4616485a051bde37
jeff.johnston.mn <jeff.johnston.mn@07444f95-541f-0410-993b-35a84c1256c0>
2010-06-22 02:47:43 +0000

    Needed to make more of the objects serializable.

--- jmesa/src/org/jmesa/limit/LimitImpl.java
+++ jmesa/src/org/jmesa/limit/LimitImpl.java
@@ -15,13 +15,14 @@
  */
 package org.jmesa.limit;
 
+import java.io.Serializable;
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 /**
  * @since 2.0
  * @author Jeff Johnston
  */
-public class LimitImpl implements Limit {
+public class LimitImpl implements Limit, Serializable {
     private final String id;
     private RowSelect rowSelect;
     private FilterSet filterSet;
