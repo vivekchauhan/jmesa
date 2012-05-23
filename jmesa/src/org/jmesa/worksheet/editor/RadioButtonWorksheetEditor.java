1d164638a1932ecf02c5acff11ac1f614d78e311
jward.dww@gmail.com <jward.dww@gmail.com@07444f95-541f-0410-993b-35a84c1256c0>
2012-05-23 15:09:08 +0000

    Created.

---
+++ jmesa/src/org/jmesa/worksheet/editor/RadioButtonWorksheetEditor.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2012 original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jmesa.worksheet.editor;
+
+import static org.jmesa.worksheet.WorksheetUtils.isRowRemoved;
+
+import org.jmesa.view.html.HtmlBuilder;
+
+/**
+ * Defines a radio button for the worksheet editor.
+ * 
+ * @since 4.0
+ * @author Jason Ward
+ */
+
+public class RadioButtonWorksheetEditor extends HtmlWorksheetEditor {
+    
+    @Override
+    public String getValueForWorksheet(Object item, String property, int rowcount) {
+
+        Object value = super.getOriginalValue(item, property, rowcount);
+        return value == null ? Boolean.FALSE.toString() : String.valueOf(value);
+    }
+    
+    @Override
+    protected String getWsColumn(Object item, String id, String property, String uniqueProperty, String uniqueValue, Object originalValue, Object changedValue) {
+
+        HtmlBuilder html = new HtmlBuilder();
+        html.input().type("radio").name(property);
+
+        boolean isChecked = originalValue != null && Boolean.parseBoolean(originalValue.toString());
+        if (isChecked) {
+            html.checked();
+        }
+
+        String unCheckFunction = "unCheckRadio_" + property + "()";
+        if (isRowRemoved(getCoreContext().getWorksheet(), getColumn().getRow(), item)) {
+            html.disabled();
+        } else {
+            html.onclick("jQuery.jmesa.submitWsCheckableColumn(this.checked, '" + id + "','" + property + "','"
+                    + uniqueProperty + "','" + uniqueValue + "');" + unCheckFunction);
+        }
+
+        html.end();
+
+        if (isChecked) {
+            html.br();
+            html.script().type("text/javascript").close();
+            html.append(" function " + unCheckFunction + " {");
+            html.append("jQuery.jmesa.submitWsCheckableColumn(false, '" + id + "','" + property + "','"
+                    + uniqueProperty + "','" + uniqueValue + "')};");
+            html.scriptEnd();
+            html.br();
+        }
+        
+        return html.toString();
+    }
+
+}
