832166e2a501369cfbb321a63192265c24dd2133
jeff.johnston.mn@gmail.com <jeff.johnston.mn@gmail.com@07444f95-541f-0410-993b-35a84c1256c0>
2012-08-20 15:25:56 +0000

    escaping fields in the javascript file

--- trunk/jmesa/resources/jquery.jmesa.js
+++ trunk/jmesa/resources/jquery.jmesa.js
@@ -7,53 +7,53 @@
         },
         setSaveToWorksheet : function(id) {
             
-            $(this.getForm(id)).find('input[name=' + id + '_sw_]').val('true');
+            $(this.getForm(id)).find('input[name="' + id + '_sw_"]').val('true');
         },
         setFilterToWorksheet : function(id) {
             
-            $(this.getForm(id)).find('input[name=' + id + '_fw_]').val('true');
-            $(this.getForm(id)).find('input[name=' + id + '_p_]').val('1');
+            $(this.getForm(id)).find('input[name="' + id + '_fw_"]').val('true');
+            $(this.getForm(id)).find('input[name="' + id + '_p_"]').val('1');
         },
         setClearToWorksheet : function(id) {
             
-            $(this.getForm(id)).find('input[name=' + id + '_cw_]').val('true');
+            $(this.getForm(id)).find('input[name="' + id + '_cw_"]').val('true');
             this.removeFilterFromWorksheet(id);
         },
         setAddRowToWorksheet : function(id) {
             
-            $(this.getForm(id)).find('input[name=' + id + '_awr_]').val('true');
+            $(this.getForm(id)).find('input[name="' + id + '_awr_"]').val('true');
         },
         setRemoveRowToWorksheet : function(id, uniqueProperty) {
             
             $.each(uniqueProperty, function(key, value) {
-                $("#"+id).parents('form').find('input[name=' + id + '_rwr_]').val(value);
+                $("#"+id).parents('form').find('input[name="' + id + '_rwr_"]').val(value);
             });
         },
         removeFilterFromWorksheet : function(id) {
             
             this.removeAllFilters(id);
-            $(this.getForm(id)).find('input[name=' + id + '_p_]').val('1');
+            $(this.getForm(id)).find('input[name="' + id + '_p_"]').val('1');
         },
         setPage : function(id, page) {
             
-            $(this.getForm(id)).find('input[name=' + id + '_p_]').val(page);
+            $(this.getForm(id)).find('input[name="' + id + '_p_"]').val(page);
         },
         setMaxRows : function(id, maxRows) {
             
-            $(this.getForm(id)).find('input[name=' + id + '_mr_]').val(maxRows);
-            $(this.getForm(id)).find('input[name=' + id + '_p_]').val('1');
+            $(this.getForm(id)).find('input[name="' + id + '_mr_"]').val(maxRows);
+            $(this.getForm(id)).find('input[name="' + id + '_p_"]').val('1');
         },
         setSort : function(id, property, position, sort) {
             
-            $(this.getForm(id)).find('input[name=' + id + '_s_' + position + '_' + property + ']').val(sort);
+            $(this.getForm(id)).find('input[name="' + id + '_s_' + position + '_' + property + '"]').val(sort);
         },
         removeAllFilters : function(id) {
             
-            $(this.getForm(id)).find('input[name^=' + id + '_f_]').each(
+            $(this.getForm(id)).find('input[name^="' + id + '_f_"]').each(
                 function(i, child) {
                     child.value = '';
                 });
-            $(this.getForm(id)).find('input[name=' + id + '_p_]').val('1');
+            $(this.getForm(id)).find('input[name="' + id + '_p_"]').val('1');
         },
         filterKeypress : function(id, e) {
             
@@ -71,11 +71,11 @@
         },
         setExport : function(id, exportType) {
             
-            $(this.getForm(id)).find('input[name=' + id + '_e_]').val(exportType);
+            $(this.getForm(id)).find('input[name="' + id + '_e_"]').val(exportType);
         },
         setExportToLimit : function(id, exportType) {
             
-            $(this.getForm(id)).find('input[name=' + id + '_e_]').val(exportType);
+            $(this.getForm(id)).find('input[name="' + id + '_e_"]').val(exportType);
         },
         submitWorksheetColumn : function(column, id, property, uniqueProperty, uniqueValue, originalValue) {
 
@@ -90,7 +90,7 @@
             data += ', "cv_" : "' + encodeURIComponent(changedValue) + '"';
             data += '}';
 
-            var contextPath = $(this.getForm(id)).find('input[name=' + id + '_ctx_]').val();
+            var contextPath = $(this.getForm(id)).find('input[name="' + id + '_ctx_"]').val();
             if (contextPath) {
                contextPath += "/";
             }
@@ -118,7 +118,7 @@
             data += ', "cv_" : "' + encodeURIComponent(changedValue) + '"';
             data += '}';
 
-            var contextPath = $(this.getForm(id)).find('input[name=' + id + '_ctx_]').val();
+            var contextPath = $(this.getForm(id)).find('input[name="' + id + '_ctx_"]').val();
             if (contextPath) {
                contextPath += "/";
             }
@@ -141,24 +141,24 @@
 
             /* export fields */
             
-            var exportType = $(this.getForm(id)).find('input[name=' + id + '_e_]').val();
+            var exportType = $(this.getForm(id)).find('input[name="' + id + '_e_"]').val();
             if (exportType) {
                 url += '&' + id + '_e_=' + exportType;
             }
 
             /* limit fields */
             
-            var maxRow = $(this.getForm(id)).find('input[name=' + id + '_mr_]').val();
+            var maxRow = $(this.getForm(id)).find('input[name="' + id + '_mr_"]').val();
             if (maxRow) {
                 url += '&' + id + '_mr_=' + maxRow;
             }
 
-            var page = $(this.getForm(id)).find('input[name=' + id + '_p_]').val();
+            var page = $(this.getForm(id)).find('input[name="' + id + '_p_"]').val();
             if (page) {
                 url += '&' + id + '_p_=' + page;
             }
             
-            $(this.getForm(id)).find('input[name^=' + id + '_f_]').each(
+            $(this.getForm(id)).find('input[name^="' + id + '_f_"]').each(
                 function(i, child) {
                     if (child.value != null && child.value != '') {
                         url += '&' + child.name + '=' + child.value;                        
@@ -167,27 +167,27 @@
 
             /* worksheet fields */
 
-            var worksheetSave = $(this.getForm(id)).find('input[name=' + id + '_ws_]').val();
+            var worksheetSave = $(this.getForm(id)).find('input[name="' + id + '_ws_"]').val();
             if (worksheetSave == 'true') {
                 url += '&' + id + '_sw_=true';
             }
 
-            var worksheetFilter = $(this.getForm(id)).find('input[name=' + id + '_wf_]').val();
+            var worksheetFilter = $(this.getForm(id)).find('input[name="' + id + '_wf_"]').val();
             if (worksheetFilter == 'true') {
                 url += '&' + id + '_fw_=true';
             }
 
-            var worksheetClear = $(this.getForm(id)).find('input[name=' + id + '_wc_]').val();
+            var worksheetClear = $(this.getForm(id)).find('input[name="' + id + '_wc_"]').val();
             if (worksheetClear == 'true') {
                 url += '&' + id + '_cw_=true';
             }
 
-            var worksheetAddRow = $(this.getForm(id)).find('input[name=' + id + '_awr_]').val();
+            var worksheetAddRow = $(this.getForm(id)).find('input[name="' + id + '_awr_"]').val();
             if (worksheetAddRow == 'true') {
                 url += '&' + id + '_awr_=true';
             }
 
-            var worksheetRemoveRow = $(this.getForm(id)).find('input[name=' + id + '_rwr_]').val();
+            var worksheetRemoveRow = $(this.getForm(id)).find('input[name="' + id + '_rwr_"]').val();
             if (worksheetRemoveRow != null && worksheetRemoveRow != '') {
                 url += '&' + id + '_rwr_=' + worksheetRemoveRow;
             }
